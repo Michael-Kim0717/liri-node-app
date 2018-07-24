b5ece179fca68d58c7b7a45c5567a21a19c209b1
Michael Kim <MichaelKim0717@gmail.com>
2018-07-24 14:30:10 -0400

    Twitter command added

--- liri.js
+++ liri.js
@@ -1,10 +1,20 @@
 /* Require Variables */
 require("dotenv").config();
-var keys = require("keys");
+var spotifyAPI = require("node-spotify-api");
+var twitterAPI = require("twitter");
+var keys = require("./keys");
 
 /* Access APIs and values */
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var spotify = new spotifyAPI({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+});
+var twitter = new twitterAPI({
+    consumer_key: keys.twitter.consumer_key,
+    consumer_secret: keys.twitter.consumer_secret,
+    access_token_key: keys.twitter.access_token_key,
+    access_token_secret: keys.twitter.access_token_secret
+});
 
 /* Error Checking */
 if (process.argv.length == 3 || process.argv.length == 4){
@@ -12,25 +22,38 @@ if (process.argv.length == 3 || process.argv.length == 4){
     if (process.argv.length == 3){
         switch (command) {
             case 'my-tweets' :
-                console.log("My Tweets :");
+                console.log("\nHere are some of your recent tweets: \n");
+                var params = {screen_name: 'i_am_loaf_cat'};
+                twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
+                    if (!error) {
+                        var length = 20;
+                        if (tweets.length < 20){
+                            length = tweets.length;
+                        }
+                        for (var i = 0; i < length; i ++){
+                            console.log((i+1) + ". Tweet: " + tweets[i].text);
+                            console.log("Created at: " + tweets[i].created_at.substring(0, 19) + "\n");
+                        }
+                    }
+                });
                 break;
             case 'do-what-it-says' :
-                console.log("Do What It Says :");
+                console.log("\nDo What It Says :");
                 break;
             default :
-                console.log("This doesn't make sense to me. Please command me with 'my-tweets', 'spotify-this-song <SONGNAME>', 'movie-this <MOVIENAME>', or 'do-what-it-says'");
+                console.log("\nThis doesn't make sense to me. \nPlease command me with 'my-tweets', 'spotify-this-song <SONGNAME>', 'movie-this <MOVIENAME>', or 'do-what-it-says'");
         }
     }
     else {
         switch (command) {
             case 'spotify-this-song' :
-                console.log("This song is : " + process.argv[3]);
+                console.log("\nThis song is : " + process.argv[3]);
                 break;
             case 'movie-this' :
-                console.log("This movie is : " + process.argv[3]);
+                console.log("\nThis movie is : " + process.argv[3]);
                 break;
             default :
-                console.log("Something isn't right here. Please command me with 'my-tweets', 'spotify-this-song <SONGNAME>', 'movie-this <MOVIENAME>', or 'do-what-it-says'");
+                console.log("\nSomething isn't right here. \nPlease command me with 'my-tweets', 'spotify-this-song <SONGNAME>', 'movie-this <MOVIENAME>', or 'do-what-it-says'");
         }
     }
 }
