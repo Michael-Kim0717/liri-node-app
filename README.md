9cce76d1bf64fa123c00a14a0a43e350347656c4
Michael Kim <MichaelKim0717@gmail.com>
2018-07-26 19:17:40 -0400

    Error checking, write to log.txt, README.md update

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# liri-node-app
+## liri-node-app
+
+### Summary
+LIRI is a Node based application that has a similar concept to Apple's SIRI, but takes in a constraint of user typed commands to perform certain actions.
+Using Node packages to retrieve data from Spotify, Twitter, and OMBD, LIRI takes in certain commands in order to send back movie details, song details, and personal twitter tweets.
+
+### Documentation
+Any command performed will be written with its results into log.txt
+
+node liri.js my-tweets :
+    Using the Twitter Node Package, we will output our most recent 20 tweets.
+
+node liri.js spotify-this-song <SONGNAME> :
+    Using the Spotify Node Package, we will output the following details of the first song found.
+        Song's artist(s)
+        Song's title
+        A link to the preview of the song
+        Song's album
+    If no song name is given, it will automatically provide the details for the Backstreet Boys "I Want It That Way"
+
+node liri.js movie-this <SONGNAME> :
+    Using the OMDB API along with the Request Node Package, we want to output the following details of the first movie found.
+        Movie's name
+        The year the movie was released
+        IMDB's personal rating of the movie
+        Rotten Tomatoes' personal rating of the movie
+        Country/Countries of where the movie was produced
+        Language(s) of the movie
+        Movie's plot
+        Movie's list of actors
+    If no movie title is given, it will provide the details for "Mr. Nobody" starring Jared Leto.
+
+node liri.js do-what-it-says :
+    The 'random.txt' file within the same folder contains a bunch of random commands separated by line breaks.
+    This command will take a random line from the file and perform that specific command.
+    These commands include but are not limited to :
+        my-tweets
+        spotify-this-song "Never Gonna Give You Up"
+        movie-this "Spiderman"
--- liri.js
+++ liri.js
@@ -18,6 +18,22 @@ var twitter = new twitterAPI({
     access_token_secret: keys.twitter.access_token_secret
 });
 
+/* Get current date and time from doing certain commands */
+var currentdate = new Date(); 
+var datetime = "performed: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+var commandString = "";
+for (var i = 2; i < process.argv.length; i++){
+    commandString += process.argv[i] + " ";
+}
+commandString += datetime;
+fs.appendFile("./log.txt", commandString + "\n", function(){});
+
 /* Error Checking */
 if (process.argv.length == 3 || process.argv.length == 4){
     var command = process.argv[2];
@@ -48,11 +64,19 @@ if (process.argv.length == 3 || process.argv.length == 4){
     }
 }
 else {
+    console.log("\n============================================================================");
+    fs.appendFile("./log.txt", "\n============================================================================", function(){});
     console.log("I do not understand this many commands. Please make this easier for me!");
+    fs.appendFile("./log.txt", "\nI do not understand this many commands. Please make this easier for me!\n", function(){});
+    console.log("============================================================================");
+    fs.appendFile("./log.txt", "============================================================================\n\n", function(){});
 }
 
 function my_tweets(){
-    console.log("\nHere are some of your recent tweets: ");
+    console.log("\n============================================================================");
+    fs.appendFile("./log.txt", "\n============================================================================\n", function(){});
+    console.log("Here are some of your recent tweets for user 'i_am_loaf_cat': ");
+    fs.appendFile("./log.txt", "Here are some of your recent tweets for user 'i_am_loaf_cat': ", function(){});
     var params = {screen_name: 'i_am_loaf_cat'};
     twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
@@ -62,9 +86,13 @@ function my_tweets(){
             }
             for (var i = 0; i < length; i ++){
                 console.log("\n" + (i+1) + ". Tweet: " + tweets[i].text);
+                fs.appendFile("./log.txt", "\n" + (i + 1) + ". Tweet: " + tweets[i].text + "\n", function(){});
                 console.log("Created at: " + tweets[i].created_at.substring(0, 19));
+                fs.appendFile("./log.txt", "Created at: " + tweets[i].created_at.substring(0, 19) + "\n", function(){});
             }
         }
+        console.log("============================================================================");
+        fs.appendFile("./log.txt", "============================================================================\n\n", function(){});
     });
 }
 
@@ -73,13 +101,20 @@ function spotify_this(song_name) {
         if (!err) {
             var songValue = data.tracks.items[0];
             console.log("Artist(s) : ");
+            fs.appendFile("./log.txt", "\nArtist(s) : \n", function(){}); 
             for (var i = 0; i < songValue.artists.length; i++){
                 console.log(songValue.artists[i].name);
+                fs.appendFile("./log.txt", songValue.artists[i].name + "\n", function(){}); 
             }
             console.log("\nSong Title : \n" + songValue.name
             + "\n\nPreview the Song Here : \n" + songValue.preview_url
             + "\n\nAlbum : \n" + songValue.album.name);
+            fs.appendFile("./log.txt", "\nSong Title : \n" + songValue.name
+            + "\n\nPreview the Song Here : \n" + songValue.preview_url
+            + "\n\nAlbum : \n" + songValue.album.name, function(){}); 
         }
+        console.log("============================================================================");
+        fs.appendFile("./log.txt", "\n============================================================================\n\n", function(){}); 
     });
 }
 
@@ -89,19 +124,28 @@ function movie_this(queryURL) {
             var movieDetails = JSON.parse(body);
             console.log("Movie Name : \n" + movieDetails.Title
             + "\n\nYear : \n" + movieDetails.Year);
+            fs.appendFile("./log.txt", "\nMovie Name : \n" + movieDetails.Title + "\n\nYear : \n" + movieDetails.Year + "\n", function(){});
             for (var i = 0; i < movieDetails.Ratings.length; i++){
                 if (movieDetails.Ratings[i].Source == "Internet Movie Database"){
                     console.log("\nIMDB Rating : \n" + movieDetails.Ratings[i].Value);
+                    fs.appendFile("./log.txt", "\nIMDB Rating : \n" + movieDetails.Ratings[i].Value + "\n", function(){});
                 }
                 else if (movieDetails.Ratings[i].Source == "Rotten Tomatoes"){
                     console.log("\nRotten Tomatoes Rating : \n" + movieDetails.Ratings[i].Value);
+                    fs.appendFile("./log.txt", "\nRotten Tomatoes Rating : \n" + movieDetails.Ratings[i].Value + "\n", function(){});
                 }
             }
             console.log("\nCountry where the movie was produced :\n" + movieDetails.Country
             + "\n\nLanguage of the Movie : \n" + movieDetails.Language
             + "\n\nPlot : \n" + movieDetails.Plot
             + "\n\nActors : \n" + movieDetails.Actors);
+            fs.appendFile("./log.txt", "\nCountry where the movie was produced :\n" + movieDetails.Country
+            + "\n\nLanguage of the Movie : \n" + movieDetails.Language
+            + "\n\nPlot : \n" + movieDetails.Plot
+            + "\n\nActors : \n" + movieDetails.Actors + "\n", function(){});
         }
+        console.log("============================================================================");
+        fs.appendFile("./log.txt", "============================================================================\n\n", function(){});
     });
 }
 
@@ -111,32 +155,54 @@ function three_arguments(command){
             my_tweets();
             break;
         case 'spotify-this-song' :
-            console.log("\nSorry, you didn't really specify anything, but take a look at this song!\n");
+            console.log("\n============================================================================");
+            fs.appendFile("./log.txt", "\n============================================================================\n", function(){}); 
+            console.log("Sorry, you didn't really specify anything, but take a look at this song!\n");
+            fs.appendFile("./log.txt", "Sorry, you didn't really specify anything, but take a look at this song!\n", function(){}); 
             spotify_this("I Want it That Way");
             break;
         case 'movie-this' :
+            console.log("\n============================================================================");
+            fs.appendFile("./log.txt", "\n============================================================================", function(){});
             var movieName = "Mr. Nobody";
             var queryURL = "https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-            console.log("\nOops, it looks like you didn't request anything. Check out this movie though, it's on Netflix!\n");
+            console.log("Oops, it looks like you didn't request anything. Check out this movie though, it's on Netflix!\n");
+            fs.appendFile("./log.txt", "\nOops, it looks like you didn't request anything. Check out this movie though, it's on Netflix!\n", function(){});
             movie_this(queryURL);
             break;
         default :
-            console.log("\nThis doesn't make sense to me. \nPlease command me with 'my-tweets', 'spotify-this-song <SONGNAME>', 'movie-this <MOVIENAME>', or 'do-what-it-says'");
+            console.log("\n============================================================================");
+            fs.appendFile("./log.txt", "\n============================================================================", function(){});
+            console.log("This doesn't make sense to me. \nPlease command me with 'my-tweets', 'spotify-this-song <SONGNAME>', 'movie-this <MOVIENAME>', or 'do-what-it-says'");
+            fs.appendFile("./log.txt", "\nThis doesn't make sense to me. \nPlease command me with 'my-tweets', 'spotify-this-song <SONGNAME>, 'movie-this <MOVIENAME>', or 'do-what-it-says'\n", function(){});
+            console.log("============================================================================");
+            fs.appendFile("./log.txt", "============================================================================\n\n", function(){});
     }
 }
 
 function four_arguments(command, argument) {
     switch (command) {
         case 'spotify-this-song' :
-            console.log("\nHere are the results from your song.\n");
+            console.log("\n============================================================================");
+            fs.appendFile("./log.txt", "\n============================================================================", function(){});
+            console.log("Here are the results from your song.\n");
+            fs.appendFile("./log.txt", "\nHere are the results from your song.\n", function(){});
             spotify_this(argument);
             break;
         case 'movie-this' :
+            console.log("\n============================================================================");
+            fs.appendFile("./log.txt", "\n============================================================================", function(){});
             var queryURL = "https://www.omdbapi.com/?t=" + argument + "&y=&plot=short&apikey=trilogy";
-            console.log("\nHere are some details about your movie!\n")
+            console.log("Here are some details about your movie!\n");
+            fs.appendFile("./log.txt", "\nHere are some details about your movie!\n", function(){});
             movie_this(queryURL);
             break;
         default :
-            console.log("\nSomething isn't right here. \nPlease command me with 'my-tweets', 'spotify-this-song <SONGNAME>', 'movie-this <MOVIENAME>', or 'do-what-it-says'");
+            console.log("\n============================================================================");
+            fs.appendFile("./log.txt", "\n============================================================================", function(){});
+            console.log("Something isn't right here. \nPlease command me with 'my-tweets', 'spotify-this-song <SONGNAME>', 'movie-this <MOVIENAME>', or 'do-what-it-says'");
+            fs.appendFile("./log.txt", "\nSomething isn't right here. \nPlease command me with 'my-tweets', 'spotify-this-song <SONGNAME>, 'movie-this <MOVIENAME>', or 'do-what-it-says'\n", function(){});
+            console.log("============================================================================");
+            fs.appendFile("./log.txt", "============================================================================\n\n", function(){});
     }
 }
